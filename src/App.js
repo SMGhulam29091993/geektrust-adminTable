0a0d30ae06960548635e812904efd1717393647e
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2024-03-11 19:37:40 +0530

    created the search bar and table to show users

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import axios from "axios";
 const App = () => {
   const [users,setUsers] = useState([]);
   const [filterUser,setFilterUser] = useState([]);
+  const [searchTerm,setSearchTerm] = useState("")
+  const [selectedRow, setSelectedRow] = useState([]);
+  const pageSize = 10;
+  const [currentPage,setCurrentPage] = useState(1);
+  const [count, setCount] = useState(0)
 
   const fetchUser = async ()=>{
     try {
@@ -21,10 +26,59 @@ const App = () => {
     fetchUser();
   },[]);
 
-  console.log(users);
-  return (
-    <div>
+  const handleFilter = (e)=>{
+    e.preventDefault();
+    const filter = users.filter(user=>
+      user.name.toLowerCase().includes(searchTerm.toLowerCase()) || 
+      user.email.toLowerCase().includes(searchTerm.toLowerCase()) || 
+      user.role.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    setFilterUser(filter);
+    setCurrentPage(1);
+  }
+ 
+  const handleSelectedRow = (id)=>{
+    
+    if(selectedRow.includes(id)){
+      setSelectedRow(selectedRow.filter(rowId=>rowId!==id));
+      setCount(count-1)
+    }else{
+      setSelectedRow([...selectedRow, id]);
+      setCount(count+1)
+    }
+  }
 
+  const totalPages = Math.ceil(filterUser.length/pageSize);
+  const startIndex = (currentPage-1)*pageSize;
+  const endIndex = Math.min(startIndex+pageSize, filterUser.length);
+
+  console.log(searchTerm);
+  return (
+    <div className=''>
+      <div className='flex mx-auto items-center max-w-7xl p-3 gap-2'>
+        <input type='text' placeholder='Search by name, email, role...'
+          value={searchTerm} onChange={(e)=>{setSearchTerm(e.target.value)}} 
+          className='border border-gray-500 rounded-lg p-2' style={{width:"90%"}}/>
+          <button onSubmit={handleFilter} className=' bg-blue-700 text-white p-2 rounded-lg w-20 sm:w-40 uppercase hover:opacity-90'>Search</button>
+      </div>
+      <div className='max-w-7xl mx-auto items-center'>
+        <div className='w-full border border-slate-500 p-4'>
+          {filterUser.slice(startIndex,endIndex).map(user=>(
+            <div key={user.id} className='flex items-center justify-between'>
+              <input type='checkbox' checked={selectedRow.includes(user.id)} onChange={()=>handleSelectedRow(user.id)}/>
+              <span>{user.name}</span>
+              <span>{user.email}</span>
+              <span>{user.role}</span>
+              <span className='flex gap-4'>
+                <span >Edit</span>
+                <span>Delete</span>
+              </span>
+            </div>
+           ))}
+          
+        </div>
+        
+      </div>
     </div>
   )
 }
