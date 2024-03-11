4faac002b9c4d64fa2b6e5a97d6ec1262391644e
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2024-03-11 15:10:48 +0530

    used axios to get users from the given api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
+
 
 const App = () => {
+  const [users,setUsers] = useState([]);
+  const [filterUser,setFilterUser] = useState([]);
+
+  const fetchUser = async ()=>{
+    try {
+      const res = await axios.get("https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json");
+      const responseData = res.data;
+      console.log(responseData);
+      setUsers(responseData);
+      setFilterUser(responseData);
+    } catch (error) {
+      console.log(`Error in fetching the users ${error}`);
+    }
+  }
+  useEffect(()=>{
+    fetchUser();
+  },[]);
+
+  console.log(users);
   return (
-    <div>App</div>
+    <div>
+
+    </div>
   )
 }
 
